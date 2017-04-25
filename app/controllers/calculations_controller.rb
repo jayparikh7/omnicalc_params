e70f949424037151a8078bc68eda3df6ad4d398a
Jay Parikh <jayparikh7@users.noreply.github.com>
2017-04-25 01:57:09 -0500

    Finished it, I think

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -38,15 +38,52 @@ class CalculationsController < ApplicationController
   end
 
   def square_form
-
-
     render("calculations/square_form.html.erb")
   end
 
   def square
-    # params = {"user_number"=>"5"}
     @the_number = params[:user_number].to_f
     render("calculations/square_results.html.erb")
+  end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @the_number = params[:user_number].to_f
+    render("calculations/square_root_results.html.erb")
+  end
+
+  def payment
+    @apr = params[:apr].to_f
+    @number_of_years = params[:number_of_years].to_f
+    @present_value = params[:present_value].to_f
+
+    rate = (@apr / 100) / 12
+    monthly = @number_of_years * 12
+
+    @payment = (rate * @present_value) / (1 - (1 + rate) ** (- monthly))
+    render("calculations/payment_results.html.erb")
+  end
+
+  def payment_form
+
+
+    render("calculations/payment_form.html.erb")
+  end
+
+  def random
+    @min = params[:min].to_f
+    @max = params[:max].to_f
+
+    @random = rand(@min..@max)
+    render("calculations/random_results.html.erb")
+  end
+
+  def random_form
+
+    render("calculations/random_form.html.erb")
   end
 
 end
