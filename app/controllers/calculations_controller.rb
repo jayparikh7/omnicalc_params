54afe49f235dd49cd8b57863d88d8fb37df483ac
Jay Parikh <jayparikh7@users.noreply.github.com>
2017-04-25 01:02:49 -0500

    Made some progress

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -14,11 +14,25 @@ class CalculationsController < ApplicationController
   end
 
   def flex_interest
+    @basis_points = params["basis_points"].to_f
+    @number_of_years = params["number_of_years"].to_f
+    @present_value = params["present_value"].to_f
+
+    @interest_rate = (@basis_points / 100)
+    rate = @interest_rate / 100 / 12
+    monthly = @number_of_years * 12
+
+    @payment = (rate * @present_value) / (1 - (1 + rate) ** (- monthly))
 
     render("calculations/flex_interest.html.erb")
   end
 
   def flex_random
+
+    @min = params["min"].to_i
+    @max = params["max"].to_i
+
+    @random = rand(@min..@max)
 
     render("calculations/flex_random.html.erb")
   end
@@ -30,9 +44,9 @@ class CalculationsController < ApplicationController
   end
 
   def square
-    # parmams = {"user_number"=>"5"}
+    # params = {"user_number"=>"5"}
     @the_number = params[:user_number].to_f
-    render("calculations/square.html.erb")
+    render("calculations/square_results.html.erb")
   end
 
 end
